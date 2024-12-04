c64702935f21a57d9181fec2dd338c1e505ccb41
Muz <syed@xendit.co>
2024-12-04 10:51:53 +0800

    Update cardsSdk/src/commonMain/kotlin/com/cards/session/cards/network/NetworkConstants.kt
    Co-authored-by: ahmadAlfhajri <82349749+ahmadAlfhajri@users.noreply.github.com>

--- cardsSdk/src/commonMain/kotlin/com/cards/session/cards/network/NetworkConstants.kt
+++ cardsSdk/src/commonMain/kotlin/com/cards/session/cards/network/NetworkConstants.kt
@@ -2,6 +2,6 @@ package com.cards.session.cards.network
 
 // TODO split this
 object NetworkConstants {
-  const val PROD = "https://api.xendit.co/sessions"
-  const val BASE_URL = "https://api.stg.tidnex.dev/v3"
+  const val PROD_URL = "https://api.xendit.co/v3"
+  const val STG_URL = "https://api.stg.tidnex.dev/v3"
 }

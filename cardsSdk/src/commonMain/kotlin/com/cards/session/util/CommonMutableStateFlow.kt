4e63a23d8c24adcbe29c513ce014f7cfb642affb
Muz <smuzani@gmail.com>
2024-12-18 12:07:19 +0800

    remove unused viewmodel + flow

--- cardsSdk/src/commonMain/kotlin/com/cards/session/util/CommonMutableStateFlow.kt
+++
@@ -1,8 +0,0 @@
-package com.cards.session.util
-
-import kotlinx.coroutines.flow.MutableStateFlow
-
-expect class CommonMutableStateFlow<T>(flow: MutableStateFlow<T>) : MutableStateFlow<T>
-
-fun <T> MutableStateFlow<T>.toCommonMutableStateFlow() =
-  com.cards.session.util.CommonMutableStateFlow(this)

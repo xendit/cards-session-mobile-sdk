fd99ae30ed7b4a10c3f0f3c3d41d9d8f18aeb0a5
Muzani <syed@xendit.co>
2025-02-11 21:19:28 +0800

    fix ios build

--- xendit_cards_plugin/ios/Classes/XenditCardsPlugin.swift
+++ xendit_cards_plugin/ios/Classes/XenditCardsPlugin.swift
@@ -1,7 +1,17 @@
 import Flutter
 import UIKit
+import cardsSdk
 
 public class XenditCardsPlugin: NSObject, FlutterPlugin {
+  private let cardSessions: CardSessions
+  private var apiKey: String = ""
+
+  init(apiKey: String) {
+    self.apiKey = apiKey
+    self.cardSessions = CardSessionsFactory().create(apiKey: apiKey)
+    super.init()
+  }
+
   public static func register(with registrar: FlutterPluginRegistrar) {
     let channel = FlutterMethodChannel(name: "xendit_cards_plugin", binaryMessenger: registrar.messenger())
     let instance = XenditCardsPlugin(apiKey: "xnd_public_development_YOUR_KEY_HERE")
@@ -10,8 +20,22 @@ public class XenditCardsPlugin: NSObject, FlutterPlugin {
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
     switch call.method {
-    case "getPlatformVersion":
-      result("iOS " + UIDevice.current.systemVersion)
+    case "collectCardData":
+      guard let args = call.arguments as? [String: Any] else {
+        result(FlutterError(code: "INVALID_ARGUMENTS", message: "Arguments are required", details: nil))
+        return
+      }
+      // Implementation will be added here
+      result(FlutterMethodNotImplemented)
+      
+    case "collectCvn":
+      guard let args = call.arguments as? [String: Any] else {
+        result(FlutterError(code: "INVALID_ARGUMENTS", message: "Arguments are required", details: nil))
+        return
+      }
+      // Implementation will be added here
+      result(FlutterMethodNotImplemented)
+      
     default:
       result(FlutterMethodNotImplemented)
     }

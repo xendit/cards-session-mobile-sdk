ac90081664baa0f2c7c4f0c7e157577cc2df4a45
Muzani <syed@xendit.co>
2025-04-16 14:29:26 +0800

    update for android

--- README.md
+++ README.md
@@ -18,7 +18,7 @@ Add the following to your app's `build.gradle.kts`:
 
 ```kotlin
 dependencies {
-    implementation("com.xendit:cardsSdk:1.0.1")
+    implementation("com.xendit:cardsSdk:1.0.2")
 }
 ```
 
--- xendit_cards_session/lib/src/network/network_constants.dart
+++ xendit_cards_session/lib/src/network/network_constants.dart
@@ -1,12 +1,6 @@
-import 'package:flutter/foundation.dart';
-
 /// Network-related constants
 class NetworkConstants {
-  /// Base URL for API requests
-  /// Returns staging URL in debug mode, production URL otherwise
   static String get baseUrl {
-    return kDebugMode 
-        ? 'https://api.stg.tidnex.dev/v3' 
-        : 'https://api.xendit.co/v3';
+    return 'https://api.xendit.co/v3';
   }
 } 
